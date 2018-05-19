2b377fd6c07c93d36fdae2aa616782e4ecf0bc01
KarimovKamil <imanymuch@gmail.com>
2018-05-19 12:18:02 +0300

    change userDto verification

--- src/main/java/ru/itis/grant/validation/dto/UserDtoValidator.java
+++ src/main/java/ru/itis/grant/validation/dto/UserDtoValidator.java
@@ -3,6 +3,7 @@ package ru.itis.grant.validation.dto;
 import ru.itis.grant.dto.request.RequestUserDto;
 import ru.itis.grant.security.exception.IncorrectDataException;
 
+import java.util.Date;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,25 +26,28 @@ public class UserDtoValidator {
 
     public boolean verify(RequestUserDto userDto) {
         if (!verifyPassword(userDto.getPassword())) {
-            return false;
+            throw new IncorrectDataException("password", "Неверно введен пароль");
         }
-
+        if (!verifyEmail(userDto.getEmail())) {
+            throw new IncorrectDataException("email", "Неверно введен email");
+        }
+        if (!verifyBirthDate(userDto.getBirthDate())) {
+            throw new IncorrectDataException("birthDate", "Неверно введена дата рождения");
+        }
         return true;
     }
 
     public boolean verifyEmail(String email) {
         Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
-        if (Objects.isNull(email) || email.length() == 0 || !matcher.matches()) {
-            throw new IncorrectDataException("email", "Неверно введен email");
-        }
-        return true;
+        return !Objects.isNull(email) && email.length() != 0 && matcher.matches();
     }
 
     public boolean verifyPassword(String password) {
-        if (Objects.isNull(password) || password.length() < 6 || password.length() > 40) {
-            throw new IncorrectDataException("password", "Неверно введен пароль");
-        }
-        return true;
+        return !Objects.isNull(password) && password.length() >= 6 && password.length() <= 40;
+    }
+
+    public boolean verifyBirthDate(Date date) {
+        return !Objects.isNull(date) && !date.after(new Date(System.currentTimeMillis()));
     }
 }
